73d91af041cfcbcfd75ac0eb11752771c8b401b3
Rischerion <matthew.schnee@gmail.com>
2018-07-22 12:32:12 -0700

    Fix PATH env variable for sub-tasks

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env node
 /* tslint:disable-next-line */
 require('yargs')
     .help()
--- src/lib/do-task.ts
+++ src/lib/do-task.ts
@@ -1,4 +1,6 @@
 import { SpawnOptions } from 'child_process';
+import * as path from 'path';
+
 import getPackageDir from './get-package-dir';
 import runProcess from './run-process';
 
@@ -6,7 +8,16 @@ export default function doTask(argv: any, taskName: string, packageName: string)
     const packageDir = getPackageDir(packageName);
     const spawnOptions: SpawnOptions = {
         cwd: packageDir,
+        env: Object.assign({}, process.env),
     };
+
+    const localBin = path.join(process.cwd(), 'node_modules', '.bin');
+    if (process.env.hasOwnProperty('PATH')) {
+        spawnOptions.env.PATH = `${process.env.PATH}${path.delimiter}${localBin}`;
+    }
+    if (process.env.hasOwnProperty('Path')) {
+        spawnOptions.env.Path = `${process.env.Path}${path.delimiter}${localBin}`;
+    }
 
     if (argv.stdio) {
         spawnOptions.stdio = 'inherit';
