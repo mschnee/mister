b8c7e7a1c00a2b72fb3aa1f6086e48fb03b948c2
Rischerion <matthew.schnee@gmail.com>
2018-08-02 15:07:07 -0700

    Clean up some documentation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import * as lib from './lib';
-export { lib };
+/**
+ * WARNING
+ * importing mister is done at your own risk.
+ *
+ * Internal functions are made available for hooking  automated tests of the compiled artifacts.
+ * There is not a stable API to be consumed.
+ */
+export * from './lib';
