5cad120c830f2fa8aa7f389300c33a2044893f4e
Rischerion <matthew.schnee@gmail.com>
2018-08-28 16:20:23 -0700

    Some cleanup

--- src/lib/stream/npm-tgz-to-zip.ts
+++ src/lib/stream/npm-tgz-to-zip.ts
@@ -3,6 +3,7 @@ import { Transform } from 'stream';
 
 import * as gunzip from 'gunzip-maybe';
 import * as tarStream from 'tar-stream';
+import * as archiver from 'archiver';
 
 /**
  * Takes a tgz created by `npm pack` and puts it's `./package/` contents into a zip file.
@@ -12,8 +13,32 @@ import * as tarStream from 'tar-stream';
 export default function npmTgzToZip(tgzFileName, zipFileName) {
     return new Promise((resolve, reject) => {
         const inStream = fs.createReadStream(tgzFileName);
+        const archive = archiver('zip', {
+            zlib: { level: 9 },
+        });
         const oStream = fs.createWriteStream(zipFileName);
 
-        inStream.pipe(gunzip()).pipe(tarStream());
+        archive.pipe(oStream);
+
+        const xstream = tarStream.extract();
+        xstream.on('entry', (header, stream, done) => {
+            archive.append(stream, { name: header.name.replace('package/', '')});
+            stream.on('end', done);
+            stream.resume();
+        });
+
+        xstream.on('finish', () => {
+            archive.finalize();
+        });
+
+        xstream.on('error', (e) => {
+            reject(e);
+        });
+
+        inStream.pipe(gunzip()).pipe(xstream);
+
+        oStream.on('close', () => {
+            resolve();
+        });
     });
 }
