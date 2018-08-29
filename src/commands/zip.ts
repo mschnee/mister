1f69b92554778b52d45b439ace903c2342814a78
Rischerion <matthew.schnee@gmail.com>
2018-08-29 14:13:14 -0700

    Working zip function

--- src/commands/zip.ts
+++ src/commands/zip.ts
@@ -9,6 +9,7 @@ import { packCommand } from './pack';
 
 import getMatchingLocalPackages from '../lib/package/get-matching-local-packages';
 import resolveDistFileLocation from '../lib/package/resolve-dist-file-location';
+import npmTgzToZip from '../lib/stream/npm-tgz-to-zip';
 
 export const command = 'zip [packages...]';
 export const describe = 'Creates a zip file with bundledDependencies.  Does not check if they are built first.';
@@ -31,17 +32,8 @@ export function zipCommand(argv) {
     });
 }
 
-/**
- * 1. Find the dist file name
- * 2. create read file stream
- * 3. gunzip
- * 4. untar
- * 5. for each tar entry, pipe into output zip stream.
- */
 function zipLocalPackage(packageName) {
-    return new Promise((resolve, reject) => {
-        const tgzFile = resolveDistFileLocation(packageName);
-        const zipFile = path.join(path.dirname(tgzFile), path.basename(tgzFile, '.tgz') + '.zip');
-        resolve();
-    });
+    const tgzFile = resolveDistFileLocation(packageName);
+    const zipFile = path.join(path.dirname(tgzFile), path.basename(tgzFile, '.tgz') + '.zip');
+    return npmTgzToZip(tgzFile, zipFile);
 }
